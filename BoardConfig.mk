cfa57665c5b409403bbd47ba073c0393a417a9da
Harsh Shandilya <msfjarvis@gmail.com>
2017-07-22 16:22:57 +0530

    oneplus3: Remove user build test from dexpreopt
    Change-Id: Ib24fc322dce51689fe3ab67796cef16e4d258bb1

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -164,20 +164,12 @@ NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
 
 # Enable dexpreopt to speed boot time
 ifeq ($(HOST_OS),linux)
-  # Only enable on user builds
-  # It's annoying to have to flash the whole rom to test things etc.
-  ifeq ($(TARGET_BUILD_VARIANT),user)
-    ifeq ($(WITH_DEXPREOPT),)
-      WITH_DEXPREOPT := true
-    endif
-  else
     # Environment variable
     ifeq ($(TARGET_FORCE_DEXPREOPT),true)
       WITH_DEXPREOPT := true
     else
       WITH_DEXPREOPT := false
     endif # TARGET_FORCE_DEXPREOPT = true
-  endif # TARGET_BUILD_VARIANT = user
 endif
 
 # GPS
